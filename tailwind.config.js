cdb3d9b7d8539decbbfea5595aac658bdb39667c
Rohan Kumar <rk399504@gmail.com>
2024-12-31 17:59:21 +0530

    Update file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html", // Include your HTML file
-    "./src/**/*.{js,jsx,ts,tsx}", // Include your JS/JSX/TS/TSX files
+    './src/**/*.{html,js,jsx,ts,tsx}',
+    './public/index.html',
   ],
   theme: {
     extend: {},
